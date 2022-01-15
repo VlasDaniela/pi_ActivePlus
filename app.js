d4f0a246c9dc119ef97e58f8c64594bb76ba8d15
VlasDaniela <95177537+VlasDaniela@users.noreply.github.com>
2022-01-15 15:17:49 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ mongoClient.connect(url, (err, db) => {
                 if (result == null) {
                     collection.insertOne(newUser, (err, result) => {
                         res.status(200).send()
+                        console.log(req.body)
                     })
                 } else {
                     res.status(400).send()
@@ -59,6 +60,34 @@ mongoClient.connect(url, (err, db) => {
 
                 } else {
                     res.status(404).send()
+                }
+
+            })
+
+        })
+
+        app.post('/newTask', (req, res) => {
+
+            const newTask = {
+                task: req.body.task,
+                task1: req.body.task1,
+                task2: req.body.task2,
+                task3: req.body.task3,
+                task4: req.body.task4,
+                task5: req.body.task5,
+            }
+
+            const query = { task: newTask.task }
+
+            collection.findOne(query, (err, result) => {
+
+                if (result == null) {
+                    collection.insertOne(newTask, (err, result) => {
+                        res.status(200).send()
+                        console.log(req.body);
+                    })
+                } else {
+                    res.status(400).send()
                 }
 
             })
